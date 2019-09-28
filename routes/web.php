1152d4b59f832bf89cd575107e0260b44b147917
nuklejp <nuklejp@yahoo.co.jp>
2019-09-28 20:31:11 +0900

    another coment out

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,4 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
